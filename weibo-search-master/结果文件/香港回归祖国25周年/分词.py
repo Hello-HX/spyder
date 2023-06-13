58971d48a41ef7f0dbbf18f0e5b26aa861a87f3f
GUN_HX <111850224+Hello-HX@users.noreply.github.com>
2023-06-13 14:04:57 +0800

    Add files via upload

---
+++ weibo-search-master/结果文件/香港回归祖国25周年/分词.py
@@ -0,0 +1,36 @@
+import re
+
+import pandas as pd
+
+# 读取数据
+df = pd.read_csv('考研复试.csv', encoding='utf-8-sig')
+df = df['微博正文']
+df.to_csv('考研复试_整合.csv', index=False, encoding='utf-8-sig')
+df = pd.read_csv('考研复试_整合.csv', encoding='utf-8-sig')
+
+
+# 分词
+import jieba
+
+# 停用词
+with open('stop.txt', encoding='utf-8') as f:
+    stopwords = f.read().splitlines()
+# 分词
+# 去掉非中文字符 去掉@网名
+
+df['微博正文'] = df['微博正文'].apply(lambda x: ''.join(re.findall('[\u4e00-\u9fa5]', x)))
+# 去掉收起 这两个字
+df['微博正文'] = df['微博正文'].apply(lambda x: x.replace('收起', ''))
+# 去掉停用词
+df['微博正文'] = df['微博正文'].apply(lambda x: [i for i in jieba.cut(x) if i not in stopwords])
+# 去掉单字
+df['微博正文'] = df['微博正文'].apply(lambda x: [i for i in x if len(i) > 1])
+df['微博正文'] = df['微博正文'].apply(lambda x: ' '.join(x))
+
+# 保存
+#df.to_csv('双减政策_合并.csv', index=False, encoding='utf-8-sig')
+
+# 保存分词后的文本到文本文件中
+with open('part.txt', 'w', encoding='utf-8-sig') as f:
+    f.write('\n'.join(df['微博正文'].tolist()))
+
